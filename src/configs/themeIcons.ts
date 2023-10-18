76627bc26f7bc6c1ac8c93845866cb251f3ae281
Hanna Tiara Andarlia <hannatiara@gmail.com>
2023-10-18 17:36:37 +0800

    Add theme icons assets

---
+++ src/configs/themeIcons.ts
@@ -0,0 +1,35 @@
+const dark = {
+  payever: require('@/assets/dark/payever.svg'),
+  apps: require('@/assets/dark/apps.svg'),
+  avatar: require('@/assets/dark/avatar.svg'),
+  dockSidebar: require('@/assets/dark/dockSidebar.svg'),
+  hamburger: require('@/assets/dark/hamburger.svg'),
+  message: require('@/assets/dark/message.svg'),
+  PayEx: require('@/assets/dark/PayEx.svg'),
+  PayPal: require('@/assets/dark/PayPal.svg'),
+  reorder: require('@/assets/dark/reorder.svg'),
+  santander: require('@/assets/dark/santander.svg'),
+  search: require('@/assets/dark/search.svg'),
+  stripe: require('@/assets/dark/stripe.svg'),
+  wiretransfer: require('@/assets/dark/wiretransfer.svg'),
+};
+
+const light = {
+  payever: require('@/assets/light/payever.svg'),
+  apps: require('@/assets/light/apps.svg'),
+  avatar: require('@/assets/light/avatar.svg'),
+  dockSidebar: require('@/assets/light/dockSidebar.svg'),
+  hamburger: require('@/assets/light/hamburger.svg'),
+  message: require('@/assets/light/message.svg'),
+  PayEx: require('@/assets/light/PayEx.svg'),
+  PayPal: require('@/assets/light/PayPal.svg'),
+  reorder: require('@/assets/light/reorder.svg'),
+  santander: require('@/assets/light/santander.svg'),
+  search: require('@/assets/light/search.svg'),
+  stripe: require('@/assets/light/stripe.svg'),
+  wiretransfer: require('@/assets/light/wiretransfer.svg'),
+};
+
+const themeIcons = { dark, light };
+
+export default themeIcons;
