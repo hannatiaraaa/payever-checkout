cb3ec5d14951f91e2bcb528bfaf7c02c18b35163
Hanna Tiara Andarlia <hannatiara@gmail.com>
2023-10-18 17:55:41 +0800

    Create Common Item component

---
+++ src/components/rows/CommonItem.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import type { TCommonItem } from '@/types/rows.type';
+import themeIcons from '@/configs/themeIcons';
+import Image from 'next/image';
+import ToggleSwitch from '@/components/buttons/ToggleSwitch';
+
+const CommonItem = ({ name, theme, checked, icon, iconKey, hasToggle, rightText }: TCommonItem) => {
+  const { dark, light } = themeIcons;
+  const themeIcon = theme === 'light' ? light[iconKey as keyof typeof light] : dark[iconKey as keyof typeof dark];
+  const [isActive, setIsActive] = useState(checked);
+
+  const onSwitch = () => setIsActive(!isActive);
+
+  return (
+    <div className='flex items-center justify-between p-row gap-2 theme-border-b'>
+      <Image
+        src={icon ?? themeIcon}
+        alt={name}
+        className='w-4 object-contain'
+      />
+      <p className='flex-1 text-base theme-text'>{name}</p>
+      <div className='flex items-center gap-2'>
+        {hasToggle ? (
+          <ToggleSwitch
+            checked={isActive}
+            onClick={onSwitch}
+          />
+        ) : null}
+        {rightText && (
+          <button disabled={hasToggle}>
+            <p className='text-base font-medium text-blue-highlight'>{rightText}</p>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CommonItem;
---
+++ src/types/rows.type.ts
@@ -0,0 +1,12 @@
+import { TThemeType } from './theme.type';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+
+export type TCommonItem = {
+  name: string;
+  icon?: StaticImport;
+  iconKey?: string;
+  theme?: TThemeType;
+  checked?: boolean;
+  hasToggle?: boolean;
+  rightText?: string;
+};
